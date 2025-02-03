45bb867491f1597d9a683e64783cc0f1ec7fe52c
BernierJ <163698865+BernierJ@users.noreply.github.com>
2025-02-02 23:18:11 -0800

    testing
    I got the hat to be a part of the enemy animations :)
    created a movement script thx to a tutorial
    Next up on my list is enemy pathfinding

---
+++ pve-fps/Assets/EnemyAnimations.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EnemyAnimations : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
