5658dcbee69570d1608291f17929b44be95e7c16
BernierJ <163698865+BernierJ@users.noreply.github.com>
2025-02-10 20:41:59 -0800

    Update EnemyStats.cs

--- pve-fps/Assets/Scenes/JosephTest/ScriptableObjects/EnemyStats.cs
+++ pve-fps/Assets/Scenes/JosephTest/ScriptableObjects/EnemyStats.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "EnemyStats", menuName = "Scriptable Objects/EnemyStats")]
+[CreateAssetMenu(fileName = "EnemyStats", menuName = "Scriptable Objects/EnemyStats", order = 1)]
 public class EnemyStats : ScriptableObject
 {
-    
+    public float health;
+    public float chaseSpeed;
+    public float knockback;
+
 }
