379f68e4f75a87920fb312d15067a562246eadc3
BernierJ <163698865+BernierJ@users.noreply.github.com>
2025-02-14 16:04:57 -0800

    enemy & weapon WIP

--- pve-fps/Assets/Scenes/JosephTest/Scripts/EnemyLife.cs
+++ pve-fps/Assets/Scenes/JosephTest/Scripts/EnemyLife.cs
@@ -6,9 +6,11 @@ public class EnemyLife : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private EnemyHat _hat;
     [SerializeField] EnemyStats _stats;
+
+    public float _health;
     void Start()
     {
-        _stats.health = 30;
+        _health = _stats.health;
     }
 
     // Update is called once per frame
@@ -19,8 +21,8 @@ public class EnemyLife : MonoBehaviour
 
     public void TakeDamage(float dmg)
     {
-        _stats.health -= dmg;
-        if (_stats.health <= 0)
+        _health -= dmg;
+        if (_health <= 0)
         {
             Die();
         }
@@ -40,10 +42,18 @@ public class EnemyLife : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Weapon")
         {
             TakeDamage(10);
         }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        
+        
+        TakeDamage(other.GetComponent<PlayerWeapon>().weaponDamage);
+        
     }
 
 }
