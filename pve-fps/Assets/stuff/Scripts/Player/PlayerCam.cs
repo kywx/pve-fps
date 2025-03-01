290ccf1d9aeffcd8f466c046c9c49bcf79b9e072
kywx <kyliehu000@gmail.com>
2025-02-28 23:47:18 -0800

    player fix
    increase camera rotation, fix hairdryer bug, pill pickup buff, decrease camera sensitivity, delete particlecollision script

--- pve-fps/Assets/stuff/Scripts/Player/PlayerCam.cs
+++ pve-fps/Assets/stuff/Scripts/Player/PlayerCam.cs
@@ -21,7 +21,7 @@ public class PlayerCam : MonoBehaviour
 
         // calculate camera rotation for looking up and down
         xRotation -= mouseY * Time.deltaTime * ySens;
-        xRotation = Mathf.Clamp(xRotation, -90f, 50f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         
         // apply this to our camera transform
         cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
