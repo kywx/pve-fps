0b9b5483365dbafc8b45700efab362e8dda651f2
kywx <kyliehu000@gmail.com>
2025-03-03 12:36:14 -0800

    edit knockback, enemy prefab, hairdryer audio
    change knockback logic, increase enemy collider, add hairdryer audio loop

--- pve-fps/Assets/stuff/Scripts/Player/PlayerMove.cs
+++ pve-fps/Assets/stuff/Scripts/Player/PlayerMove.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerMove : MonoBehaviour
 {
     private CharacterController controller;
     private ParticleSystem part;
+
+    [SerializeField]
+    private AudioSource hairdryerStart;
+    [SerializeField]
+    private AudioSource hairdryerLoop;
+    [SerializeField]
+    private AudioSource hairdryerEnd;
+
     private Vector3 playerVelocity;
     private bool isGrounded;
 
     public float speed = 5f;
     public float gravity = -9.8f;
     public float jumpHeight = 1.5f;
+
+    private bool hairDryerOn;
+    private bool hairDryerLoopOn;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,12 +29,20 @@ public class PlayerMove : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         part = GetComponentInChildren<ParticleSystem>();
+        hairDryerOn = false;
+        hairDryerLoopOn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = controller.isGrounded;
+        if (hairDryerOn && !hairDryerLoopOn && hairdryerLoop != null && hairdryerStart != null && !hairdryerStart.isPlaying)
+        {
+            // if hairdryer is on and the start audio ends, play the loop
+            hairdryerLoop.Play();
+            hairDryerLoopOn = true;
+        }
     }
 
     public void ProcessMove(Vector2 input)
@@ -60,10 +80,25 @@ public class PlayerMove : MonoBehaviour
     public void HairdryON()
     {
         part.Play();
+        hairDryerOn = true;
+        if (hairdryerStart != null)
+        {
+            hairdryerStart.Play();
+        }
     }
 
     public void HairdryOFF()
     {
         part.Stop();
+        hairDryerOn = false;
+        if (hairdryerLoop != null)
+        {
+            hairdryerLoop.Stop();
+            hairDryerLoopOn = false;
+            if (hairdryerEnd != null)
+            {
+                hairdryerEnd.Play();
+            }
+        }
     }
 }
