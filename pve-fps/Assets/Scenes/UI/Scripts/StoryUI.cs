1efc182d86406789e2b5d105fab97637f3f03f57
BernierJ <163698865+BernierJ@users.noreply.github.com>
2025-03-11 08:42:25 -0700

    made Story buttons access the StoryUI script's shift methods

---
+++ pve-fps/Assets/Scenes/UI/Scripts/StoryUI.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StoryUI : MonoBehaviour
+{
+    public Button skip;
+    public Button next;
+    public Button prev;
+    public GameObject pagesObject;
+    private GameObject[] pageList;
+    private int page;
+    private int firstIndex = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        page = firstIndex; // start on the first page
+        // subscribe buttons
+        skip.onClick.AddListener(() => SkipStory());
+        prev.onClick.AddListener(() => ShiftLeft());
+        next.onClick.AddListener(() => ShiftRight());
+        Image[] stuff = pagesObject.GetComponentsInChildren<Image>(true);
+        pageList = new GameObject[stuff.Length];
+        int i = 0;
+        foreach (Image thing in stuff)
+        {
+            //pageList.Append(thing.gameObject);
+            pageList[i] = thing.gameObject;
+            i++;
+        }
+
+    }
+
+
+    public void SkipStory()
+    {
+        SceneManager.LoadScene("MainLevel");
+        //this.gameObject.SetActive(false);
+        //ChangePage(page, firstIndex);
+        //page = firstIndex;
+    }
+
+    public void ShiftLeft()
+    {
+        if (page > firstIndex)
+        {
+            ChangePage(page, page - 1);
+            page -= 1;
+        }
+    }
+
+    public void ShiftRight()
+    {
+        if (page == pageList.Length - 1)
+        {
+            SceneManager.LoadScene("MainLevel");
+        }
+        if (page < pageList.Length - 1)
+        {
+            ChangePage(page, page + 1);
+            page += 1;
+        }
+    }
+
+    private void ChangePage(int old_index, int new_index)
+    {
+        // change page based on index
+        pageList[old_index].SetActive(false);
+        pageList[new_index].SetActive(true);
+        // set buttons (if page = 0, set left to inactive, if page = pageList.Length-1, set right to inactive)
+
+    }
+}
